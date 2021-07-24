1f7721eae1839401d409f8a5b710a2b108c2c814
Mingxin Li <mingxin9@illinois.edu>
2021-07-24 11:48:45 -0500

    completed module 3 challenge. link: https://www.linkedin.com/learning/flutter-part-03-flutter-widgets/tapping-into-widget-properties?u=43607124

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,56 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                // can only have single-child
+                width: 100,
+                color: Colors.red,
+                //child: Text('Container 1'),
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    // can only have single-child
+                    height: 100,
+                    width: 100,
+                    color: Colors.yellow,
+                    //child: Text('Container 2'),
+                  ),
+                  Container(
+                    // can only have single-child
+                    height: 100,
+                    width: 100,
+                    color: Colors.green,
+                    //child: Text('Container 2'),
+                  ),
+                ],
+              ),
+              Container(
+                // can only have single-child
+                width: 100,
+                color: Colors.blue,
+                //child: Text('Container 3'),
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
